434029374497bb85e770d0abe31b000da82607bf
lucky-dog-w <1069849274@qq.com>
2020-03-11 21:29:19 +0800

    提交111

---
+++ gittest_one/src/main/java/com/lucky/dog/gittest_one/GittestOneApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.dog.gittest_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestOneApplication.class, args);
+	}
+
+}
