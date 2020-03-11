1a3ace0f114021e1243217c354f8d2ab3921b5e5
lucky-dog-w <55725125+lucky-dog-w@users.noreply.github.com>
2020-03-11 22:21:48 +0800

    Update and rename gittest_one/src/main/java/com/lucky/dog/gittest_one/GittestOneApplication.java to  gittest_one/ src/ main/ java/ com/ lucky/ dog/ gittest_one/GittestOneApplication.java

--- gittest_one/ src/ main/ java/ com/ lucky/ dog/ gittest_one/GittestOneApplication.java
+++ gittest_one/ src/ main/ java/ com/ lucky/ dog/ gittest_one/GittestOneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GittestOneApplication {
 
 	public static void main(String[] args) {
+System.out.println("11111");
 		SpringApplication.run(GittestOneApplication.class, args);
 	}
 
